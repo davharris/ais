e4c953b63056d7fe21123008e35a35ecbfee5bcb
davharris <davharris@ucdavis.edu>
2015-01-05 21:30:33 -0800

    tweaks to test.R

--- test.R
+++ test.R
@@ -1,9 +1,17 @@
+# This example uses an "easy" t distribution with many degrees of freedom as
+# a baseline for estimating the area under a "harder" t distribution with fewer
+# degrees of freedom, in N dimensions.
+
+# I compare the AIS approach with a standard importance sampler and usually find
+# that AIS works better, depending on various details and how the random number
+# generator behaves.
+
 set.seed(1)       # Seed the random number generator for reproducibility
 
 df = 2            # Degrees of freedom in the "hard" t distribution
 dfa = 20          # Degrees of freedom in the "easy" t distribution
-n = 5             # Number of dimensions of distribution
-K = 1000          # Number of annealed transitions
+n = 10            # Number of dimensions of distribution
+K = 10000         # Number of annealed transitions per run
 replicates = 100  # Number of AIS runs
 
 
@@ -30,10 +38,14 @@ ais_weights = AIS(
   fa = fa, 
   fb = fb, 
   transition = metropolis, 
-  jump = function(){rnorm(n)}
+  jump = function(){rt(n, (df + dfa) / 2)}
 )
 
-# Collect importance weights using non-annealed importance sampling
+# Collect importance weights using non-annealed importance sampling.
+# Note that this sampler collects K times more samples than the AIS sampler,
+# which should make them roughly similar in terms of total computational complexity.
+# The samples from AIS tend to be higher quality, even though they're each more 
+# difficult to compute.
 s = matrix(rt(K * n * replicates, df = dfa), ncol = n)
 is_weights = exp(rowSums(dt(s, df = df, log = TRUE) - dt(s, df = dfa, log = TRUE)))
 
@@ -43,5 +55,5 @@ is_weights = exp(rowSums(dt(s, df = df, log = TRUE) - dt(s, df = dfa, log = TRUE
 mean(ais_weights)  # Should be close to 1
 mean(is_weights)   # Should also be close to 1, but will usually be too low
 ########
-sd(ais_weights) / sqrt(length(ais_weights))
-sd(is_weights) / sqrt(length(is_weights))
+sd(ais_weights) / sqrt(length(ais_weights)) # Estimated standard error (hopefully reliable)
+sd(is_weights) / sqrt(length(is_weights))   # Estimated standard error (unreliable)
